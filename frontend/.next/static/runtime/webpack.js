2a2d1426518579ef1fcbd7b18fec4afb4f46a3ec
morgancostigan <morgan.costigan@gmail.com>
2020-05-05 18:41:03 -0500

    created a Meta file and imported it into Page.js

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d7fec597e52982a67a5c";
+/******/ 	var hotCurrentHash = "9244ee9b0f0ecb4bbf55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'; 
 import Header from '../components/Header';
+import Meta from './Meta';
 
 class Page extends Component {
     render() {
         return (
             <div>
+                <Meta />
                 <Header />
                 {this.props.children}
                 <p>I'm everywhere all at once.</p>
