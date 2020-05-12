cd8b6b9c001496bbb1c2d3ffa09d147ac4c60575
morgancostigan <morgan.costigan@gmail.com>
2020-05-12 15:14:19 -0500

    index.js now linked to graphQL server, setting up security config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ Path: 'variables.env' });
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//TODO  use express middleware to handle cookies (JSON Web Tokens)
+//TODO  use express middleware to populate current user
+server.start({
+    cors: {
+        credentials: true,  //authenticated users only
+        origin: process.env.FRONTEND_URL, //ensures only calls from this application are allowed through
+    }
+}, dirtyDeets => {
+    console.log(`Server is now running on port: http://localhost:${dirtyDeets.port}`);
+    
+})
+
