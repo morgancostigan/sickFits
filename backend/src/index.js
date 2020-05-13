9f8802677d11757c5f1c7da24b6bbcc5768a9ed6
morgancostigan <morgan.costigan@gmail.com>
2020-05-13 13:44:20 -0500

    fixed a typo in index.js that was preventing Primsa endpoint from being found

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-require('dotenv').config({ Path: 'variables.env' });
-const createServer = require('createServer');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 //TODO  use express middleware to handle cookies (JSON Web Tokens)
 //TODO  use express middleware to populate current user
-server.start({
-    cors: {
-        credentials: true,  //authenticated users only
-        origin: process.env.FRONTEND_URL, //ensures only calls from this application are allowed through
+server.start(
+    {
+        cors: {
+            credentials: true,  //authenticated users only
+            origin: process.env.FRONTEND_URL, //ensures only calls from this application are allowed through
+        }
+    }, 
+    dirtyDeets => {
+        console.log(`Server is now running on port: http://localhost:${dirtyDeets.port}`);
     }
-}, dirtyDeets => {
-    console.log(`Server is now running on port: http://localhost:${dirtyDeets.port}`);
-    
-})
+);
 
