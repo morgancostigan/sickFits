9edb526bae6d62f19bc1aa588d7aa86169ef1065
morgancostigan <morgan.costigan@gmail.com>
2020-05-13 14:01:03 -0500

    built a dogs query in schema.graphql and query.js

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, info ) {// dogs() = dogs: function()
+        return [{name: 'Lazlo'}, {name: 'Nadja'}];
+    }
+};
 
 module.exports = Query;
