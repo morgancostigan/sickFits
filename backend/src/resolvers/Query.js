341e228a865bb9927ea49e303533273179c727c9
morgancostigan <morgan.costigan@gmail.com>
2020-05-19 17:36:11 -0500

    changed forwardto to forwardTo in Query.js resolving nodemon error

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
-const { forwardto } = require('prisma-binding');
+const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    items: forwardto('db'), //this directs the request through prisma.graphql so you don't need to rewrite it
+    items: forwardTo('db'), 
+    //this directs the request through prisma.graphql so you don't need to rewrite it
 
     // async items(parent, args, ctx, info) {
     //     const items =  ctx.db.query.items();
