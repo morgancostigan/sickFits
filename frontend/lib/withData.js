10141304190664263af46c4a6f800c9d5e00940f
morgancostigan <morgan.costigan@gmail.com>
2020-05-20 16:27:50 -0500

    commenting next-with-apo-boost

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,6 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from 'next-with-apollo'; //works with next.js and apollo
+import ApolloClient from 'apollo-boost'; //includes all of the apollo Links
+import { endpoint } from '../config'; 
 
 function createClient({ headers }) {
   return new ApolloClient({
