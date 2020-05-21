8f7f09012fe70ed2ac211158979d686204f26f42
morgancostigan <morgan.costigan@gmail.com>
2020-05-21 14:03:33 -0500

    created an items.js file

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "438dacc2582841d3c2b0";
+/******/ 	var hotCurrentHash = "6a52d463386582e7b83a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
