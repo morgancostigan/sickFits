697f38515476a3dbf8bbb40226f7b463d780b153
morgancostigan <morgan.costigan@gmail.com>
2020-05-27 13:48:38 -0500

    Query to get item id in UpdateItem

--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
 
+const SINGLE_ITEM_QUERY = gql`
+    query SINGLE_ITEM_QUERY($id: ID!){
+        item(where: { id: $id }){
+            id
+            title
+            description
+            price
+        }
+    }
+`;
 
 const UPDATE_ITEM_MUTATION = gql`
     mutation UPDATE_ITEM_MUTATION(
@@ -28,13 +38,7 @@ const UPDATE_ITEM_MUTATION = gql`
 `;// end const UPDATE_ITEM_MUTATION
 
 class UpdateItem extends Component {
-    state = {
-        title: '',
-        description: '',
-        image: '',
-        largeImage: '',
-        price: 0
-    };
+    state = {};
 
     handleChange = (e) => {
         const { name, type, value } = e.target;
@@ -47,68 +51,74 @@ class UpdateItem extends Component {
 
     render() {
         return (
-            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
-                {(createItem, { loading, error }) => (
+            <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+                {({ data, loading }) => {
+                    return (
+                        <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+                            {(createItem, { loading, error }) => (
 
-                    <Form onSubmit={async e => {
-                        //TODO - Wait for image upload before submission
+                                <Form onSubmit={async e => {
+                                    //TODO - Wait for image upload before submission
 
-                        //vvv stop form from submitting vvv
-                        e.preventDefault();
-                        //vvv call the mutation vvv
-                        const res = await createItem();
-                        //vvv move user to single item page for item they just submitted vvv
-                        console.log({res});
-                        Router.push({
-                            pathname: '/item',
-                            query: {id: res.data.createItem.id },
-                        })
-                    }}>
-                        <Error error={error}/>
-                        <fieldset disabled={loading} aria-busy={loading}>
-                            
-                            <label htmlFor="title">
-                                Title
-                                <input
-                                    type="text"
-                                    id="title"
-                                    name="title"
-                                    placeholder="Title"
-                                    required
-                                    value={this.state.title}
-                                    onChange={this.handleChange}
-                                />
-                            </label>
-                            <label htmlFor="price">
-                                Price
-                                <input
-                                    type="number"
-                                    id="price"
-                                    name="price"
-                                    placeholder="Price"
-                                    required
-                                    value={this.state.price}
-                                    onChange={this.handleChange}
-                                />
-                            </label>
-                            <label htmlFor="description">
-                                <textarea
-                                    type="text"
-                                    id="description"
-                                    name="description"
-                                    placeholder="Tell us about this thing."
-                                    required
-                                    value={this.state.description}
-                                    onChange={this.handleChange}
-                                />
-                            </label>
-                            <button type="submit">Submit It!</button>
-                        </fieldset>
-                    </Form>
-                )}
-            </Mutation>
+                                    //vvv stop form from submitting vvv
+                                    e.preventDefault();
+                                    //vvv call the mutation vvv
+                                    const res = await createItem();
+                                    //vvv move user to single item page for item they just submitted vvv
+                                    console.log({ res });
+                                    Router.push({
+                                        pathname: '/item',
+                                        query: { id: res.data.createItem.id },
+                                    })
+                                }}>
+                                    <Error error={error} />
+                                    <fieldset disabled={loading} aria-busy={loading}>
+
+                                        <label htmlFor="title">
+                                            Title
+                                            <input
+                                                type="text"
+                                                id="title"
+                                                name="title"
+                                                placeholder="Title"
+                                                required
+                                                value={this.state.title}
+                                                onChange={this.handleChange}
+                                            />
+                                        </label>
+                                        <label htmlFor="price">
+                                            Price
+                                            <input
+                                                type="number"
+                                                id="price"
+                                                name="price"
+                                                placeholder="Price"
+                                                required
+                                                value={this.state.price}
+                                                onChange={this.handleChange}
+                                            />
+                                        </label>
+                                        <label htmlFor="description">
+                                            <textarea
+                                                type="text"
+                                                id="description"
+                                                name="description"
+                                                placeholder="Tell us about this thing."
+                                                required
+                                                value={this.state.description}
+                                                onChange={this.handleChange}
+                                            />
+                                        </label>
+                                        <button type="submit">Submit It!</button>
+                                    </fieldset>
+                                </Form>
+                            )}
+                        </Mutation>
+                    )//end Query return
+                }}
+            </Query>
         )// end return
-    }
+    }//end render
 }// end class UpdateItem extends Component
 
 export default UpdateItem;
