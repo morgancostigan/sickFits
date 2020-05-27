8a4e073680a8b70231c17549c8ad144c0caea993
morgancostigan <morgan.costigan@gmail.com>
2020-05-27 13:15:05 -0500

    updated Query.js to handle single item query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
     items: forwardTo('db'), 
+    item: forwardTo('db'), 
     //this directs the request through prisma.graphql so you don't need to rewrite it
 
     // async items(parent, args, ctx, info) {
