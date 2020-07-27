012809d8f13a3f9f91426d09d2d69260044bf6d9
morgancostigan <morgan.costigan@gmail.com>
2020-07-27 13:07:37 -0500

    created DeleteItem.js component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "30b13387bf23c2738f21";
+/******/ 	var hotCurrentHash = "53725e4142ef746ee906";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
