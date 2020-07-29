250fbdeedb407ec60597a553ac4c1ccbf9821208
morgancostigan <morgan.costigan@gmail.com>
2020-07-29 13:32:41 -0500

    built a Pagination component and imported the pagination styles

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+const Pagination = props => (
+    <PaginationStyles>
+        <p>Yo! Pagination!
+        </p>
+    </PaginationStyles>
+)
+
+export default Pagination;
