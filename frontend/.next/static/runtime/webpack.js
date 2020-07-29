107a4f0ac873742d51ca7078476e979f25e59e15
morgancostigan <morgan.costigan@gmail.com>
2020-07-29 17:04:04 -0500

    page numbers now displaying on title head

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d011e6c136de428a811c";
+/******/ 	var hotCurrentHash = "62bc47e47f250167fddd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -3,6 +3,7 @@ import PaginationStyles from './styles/PaginationStyles';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { perPage } from '../config';
+import Head from 'next/head'; 
 
 const PAGINATION_QUERY = gql`
  query PAGINATION_QUERY{
@@ -15,18 +16,25 @@ const PAGINATION_QUERY = gql`
 `;
 
 const Pagination = props => (
-    <PaginationStyles>
-        <Query query={PAGINATION_QUERY}>
-            {({data, loading, error}) => {
-                if (loading) return <p>Loading...</p>;
-                const count = data.itemsConnection.aggregate.count;
-                const pages = Math.ceil(count / perPage);
-                return (
-                <p>Page {props.page} of {pages}</p>
-            )}}
-        </Query>
-        
-    </PaginationStyles>
+
+    <Query query={PAGINATION_QUERY}>
+        {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            const count = data.itemsConnection.aggregate.count;
+            const pages = Math.ceil(count / perPage);
+            const page = props.page;
+            return (
+                <PaginationStyles>
+                    <Head>
+                        <title>SF! Page {page} of {pages}</title>
+                    </Head>
+                    <p>Page {props.page} of {pages}</p>
+                </PaginationStyles>
+            )
+        }}
+    </Query>
+
+
 )
 
 export default Pagination;
