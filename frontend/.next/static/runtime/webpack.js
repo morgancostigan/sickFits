25dc4ec006567ee71a7f962c34e3ca2eecab8796
morgancostigan <morgan.costigan@gmail.com>
2020-07-29 16:08:41 -0500

    updated items.js

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6c7ce6fe4326072fc5b3";
+/******/ 	var hotCurrentHash = "a4a854595d6dee85e188";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
