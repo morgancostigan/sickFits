c1e62d96d2884e74f03971d826f545941e9708ad
morgancostigan <morgan.costigan@gmail.com>
2020-08-21 17:48:14 -0500

    using cookieParser on express server to access cookies as an object in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-//TODO  use express middleware to handle cookies (JSON Web Tokens)
+//DONE vvvv  use express middleware to handle cookies (JSON Web Tokens)
+server.express.use(cookieParser( )); //allows access to cookies as an object
 //TODO  use express middleware to populate current user
 server.start(
     {
