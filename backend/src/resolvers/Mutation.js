397441325c7928743ed7817bae4bd86b52965e3b
morgancostigan <morgan.costigan@gmail.com>
2020-08-21 18:09:17 -0500

    built out signup resolver in Mutation.js using bcrypt for hashing password and jwt for cookie handling

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         //TODO - check if they are logged in
@@ -32,7 +35,30 @@ const Mutations = {
         //TODO
         //3. delete
         return ctx.db.mutation.deleteItem ({ where }, info); 
-    }
+    },
+
+    async signup(parent, args, content, info) {
+        args.email = args.email.toLowerCase();
+        //hash password
+        const password = await bcrypt.hash(args.password, 15);
+        //create user in DB
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER']},
+            },
+        }, info);
+        //create JWT (JSON web token) to show that user is now signed in
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        //set JWT as a cookie on response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 52, //sets cookie for one year
+        });
+        //return user to the browser
+        return user;
+    },
 };
 
 module.exports = Mutations;
