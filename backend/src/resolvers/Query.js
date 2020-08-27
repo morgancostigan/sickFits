a3228f88577741dd33897dda3bd76b6838c2ae56
morgancostigan <morgan.costigan@gmail.com>
2020-08-27 12:50:39 -0500

    built a 'me' resolver in Query.js that will retunr the userId

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -5,11 +5,16 @@ const Query = {
     item: forwardTo('db'),  
     itemsConnection: forwardTo('db'),
     //this directs the request through prisma.graphql so you don't need to rewrite it
+    me(parent, args, ctx, info) {
+        //check if there's a current userId
+        if(!ctx.request.userId) {
+            return null; 
+        }
+        return ctx.db.query.user({
+            where: {id: ctx.request.userId},
+        }, info )
+    },
 
-    // async items(parent, args, ctx, info) {
-    //     const items =  ctx.db.query.items();
-    //     return items;
-    // }
 };
 
 module.exports = Query;
