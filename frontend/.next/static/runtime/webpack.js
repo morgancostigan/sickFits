6127b8d88d81ea1d808b8c5d3af952fe24e7a9cc
morgancostigan <morgan.costigan@gmail.com>
2020-08-28 15:50:23 -0500

    updates Nav.js based on whether a use a user is signed in

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ce7138952c2a3116643a";
+/******/ 	var hotCurrentHash = "d84372d483208c5bdd39";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
