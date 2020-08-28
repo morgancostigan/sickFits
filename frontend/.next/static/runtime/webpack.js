2df897d0038fa207587aa42219ac6442b7d53fc1
morgancostigan <morgan.costigan@gmail.com>
2020-08-28 15:08:47 -0500

    copied and modified Signup.js to Signin.js and imported it into signup.js

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "12f1cacb28f18b51fffe";
+/******/ 	var hotCurrentHash = "820e3cf3c43e9e3993f5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
