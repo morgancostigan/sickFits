4839353c7e8ff39966c74ffb580ae023a76153ed
morgancostigan <morgan.costigan@gmail.com>
2020-09-03 12:07:50 -0500

    copied all of ResetRquest.js to new Reset.js to begin refactoring

---
+++ frontend/components/Reset.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+    mutation REQUEST_RESET_MUTATION( $email: String! ) {
+        requestReset ( email: $email ) {
+            message 
+        }
+    }
+`;
+
+class RequestReset extends Component {
+    state = {
+        email: ''
+    };
+    saveToState = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        return (
+            <Mutation
+                mutation={REQUEST_RESET_MUTATION}
+                variables={this.state}
+            >
+                {(reset, { error, loading, called }) => {
+                    return (
+                        <Form method="post"
+                            onSubmit={async e => {
+                                e.preventDefault();
+                                const res = await reset();
+                                console.log(res);
+                                this.setState({ email: '' });
+                            }}>
+                            <fieldset disabled={loading} aria-busy={loading}>
+                                <h2>Reset Your Password</h2>
+                                <Error error={error} />
+                                {!error && !loading && called && <p>Reset link sent to your email. Dig.</p>}
+                                <label htmlFor="email">
+                                    Email
+                                <input
+                                        type="email"
+                                        name="email"
+                                        placeholder="email"
+                                        value={this.state.email}
+                                        onChange={this.saveToState}
+                                    />
+                                </label>
+                                <button type="submit">Request Reset</button>
+                            </fieldset>
+                        </Form>)
+                }}
+            </Mutation>
+        )
+    }
+};
+
+export default RequestReset;
