e2ade5df78629e3601a8b5c705d0e19f230459f5
morgancostigan <morgan.costigan@gmail.com>
2020-09-07 19:50:03 -0500

    created PleaseSignIn compo

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "70ef3c42dbf35baf3c3a";
+/******/ 	var hotCurrentHash = "ca5b2dc90f15948aa653";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/components/PleaseSignIn.js
@@ -0,0 +1,8 @@
+import { Query } from 'react-apollo';
+import { CURRENT_USER_QUERY } from './User';
+import { Signin } from './Signin';
+
+const PleaseSignIn = (props) =>
+    <p>Please Sign In!</p>;
+
+export default PleaseSignIn;
