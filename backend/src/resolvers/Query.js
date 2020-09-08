cda920a5dc8939320b82a4835e2d61cf82c561df
morgancostigan <morgan.costigan@gmail.com>
2020-09-07 20:30:48 -0500

    created new users resolver in Query.js that checks that there is a logged in user, who has permission to query all users and if so, returns all users

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermissions } = require('../utils');
 
 const Query = {
     items: forwardTo('db'), 
@@ -13,8 +14,17 @@ const Query = {
         return ctx.db.query.user({
             where: {id: ctx.request.userId},
         }, info )
-    },
-
+    }, //end me
+    async users(parent, args, ctx, info) {
+        //1 check that user is logged in
+        if(!ctx.request.userId){
+            throw new Error('You must be logged in to do that!');
+        }
+        //2 check if user has permission to query all users
+        hasPermissions(ctx.request.user, ['ADMIN', 'PERMISSION_UPDATE']);
+        //3 if yeah, query all users dammit
+        return ctx.db.users({}, info); 
+    }, //end users
 };
 
 module.exports = Query;
