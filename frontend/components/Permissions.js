8bb3281a4b9c29144d7555427e585db439b52e04
morgancostigan <morgan.costigan@gmail.com>
2020-09-07 20:40:32 -0500

    created Permissions.js component that will run the ALL_USERS_QUERY which will be written soon, or errors if permissions are lacking

---
+++ frontend/components/Permissions.js
@@ -0,0 +1,15 @@
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+
+const Permissions = (props) => (
+    <Query query={ALL_USERS_QUERY}>
+        {({data, loading, error}) => (
+            <div>
+                <Error error={error} />
+                <p>Hey Hey Hey!!!</p>
+            </div>
+        )}
+    </Query>
+);
+
+export default Permissions;
