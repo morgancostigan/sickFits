c5e1465f97f9fb10a0f32aa6a7f025541a8e0635
morgancostigan <morgan.costigan@gmail.com>
2020-09-14 16:17:18 -0500

    built CartItem.js, imported React, formatMoney and styled, exported

---
+++ frontend/components/CartItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import formatMoney from '../lib/formatMoney';
+import styled from 'styled-components';
+
+const CartItemStyles = styled.li `
+
+`;//end CartItemStyles
+
+const CartItem = (props) => {
+    <CartItemStyles>Kia Ora!</CartItemStyles>
+};// end CartItem 
+
+export default CartItem;
